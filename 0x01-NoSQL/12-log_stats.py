26f5e63e331ee630f8526721c156471c9f9b68ec
Omar Jammeh <orms45@gmail.com>
2024-08-30 10:41:56 +0000

    import execution fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,31 +2,33 @@
 """ 12-log_stats """
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-log_collection = client.logs.nginx
-get_status = {"method": "GET", "path": "/status"}
-log_count = log_collection.count_documents({})
-get_count = log_collection.count_documents({"method": "GET"})
-post_count = log_collection.count_documents({"method": "POST"})
-put_count = log_collection.count_documents({"method": "PUT"})
-patch_count = log_collection.count_documents({"method": "PATCH"})
-delete_count = log_collection.count_documents({"method": "DELETE"})
-get_status = log_collection.count_documents(get_status)
 
-first_line = "{} logs".format(log_count)
-second_line = "Method:"
-get_requests = "\tmethod GET: {}".format(get_count)
-post_requests = "\tmethod POST: {}".format(post_count)
-put_requests = "\tmethod PUT: {}".format(put_count)
-patch_requests = "\tmethod PATCH: {}".format(patch_count)
-delete_requests = "\tmethod DELETE: {}".format(delete_count)
-get_status = "{} status check".format(get_status_count)
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_collection = client.logs.nginx
+    get_status = {"method": "GET", "path": "/status"}
+    log_count = log_collection.count_documents({})
+    get_count = log_collection.count_documents({"method": "GET"})
+    post_count = log_collection.count_documents({"method": "POST"})
+    put_count = log_collection.count_documents({"method": "PUT"})
+    patch_count = log_collection.count_documents({"method": "PATCH"})
+    delete_count = log_collection.count_documents({"method": "DELETE"})
+    get_status = log_collection.count_documents(get_status)
 
-print(f"""{first_line}
-{second_line}
-{get_requests}
-{post_requests}
-{put_requests}
-{patch_requests}
-{delete_requests}
-{get_status}""")
+    first_line = "{} logs".format(log_count)
+    second_line = "Method:"
+    get_requests = "\tmethod GET: {}".format(get_count)
+    post_requests = "\tmethod POST: {}".format(post_count)
+    put_requests = "\tmethod PUT: {}".format(put_count)
+    patch_requests = "\tmethod PATCH: {}".format(patch_count)
+    delete_requests = "\tmethod DELETE: {}".format(delete_count)
+    get_status = "{} status check".format(get_status_count)
+
+    print(f"""{first_line}
+    {second_line}
+    {get_requests}
+    {post_requests}
+    {put_requests}
+    {patch_requests}
+    {delete_requests}
+    {get_status}""")
